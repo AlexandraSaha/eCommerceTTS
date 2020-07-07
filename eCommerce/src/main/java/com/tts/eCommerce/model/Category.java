bfeeaf4b0facc71bf48b55553b86dedeab9eed65
Alexandra Saha <66027074+AlexandraSaha@users.noreply.github.com>
2020-07-07 13:08:19 -0400

    Add files via upload

--- eCommerce/src/main/java/com/tts/eCommerce/model/Category.java
+++ eCommerce/src/main/java/com/tts/eCommerce/model/Category.java
@@ -1,1 +1,54 @@
 package com.tts.eCommerce.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Category {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Long id;
+  
+  @NotEmpty(message = "Category must have a name")
+  private String name;
+  private String description;
+  
+  public Long getId() {
+    return id;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public String getDescription() {
+    return description;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public void setDescription(String description) {
+    this.description = description;
+  }
+  
+  public Category(Long id, String name,
+      String description) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+  }
+  
+  public Category() {}
+  
+  @Override
+  public String toString() {
+    return "Category [id=" + id + ", name=" + name
+        + ", description=" + description + "]";
+  }
+  
+}
--- eCommerce/src/main/java/com/tts/eCommerce/model/User.java
+++ eCommerce/src/main/java/com/tts/eCommerce/model/User.java
@@ -1,1 +1,115 @@
 package com.tts.eCommerce.model;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+public class User {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  @Column(name="user_id")
+  private Long id;
+  
+  private String email;
+  private String firstName;
+  private String lastName;
+  private String username;
+  private String password;
+  
+  public Long getId() {
+    return id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public Date getCreatedAt() {
+    return createdAt;
+  }
+
+  public Set<Role> getRoles() {
+    return roles;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public void setCreatedAt(Date createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public void setRoles(Set<Role> roles) {
+    this.roles = roles;
+  }
+
+  @CreationTimestamp
+  private Date createdAt;
+  
+  @ManyToMany(cascade = CascadeType.ALL)
+  @JoinTable(name = "user_role", joinColumns = @JoinColumn(name="user_id"), 
+      inverseJoinColumns = @JoinColumn(name = "role_id)"))
+  private Set<Role> roles;
+
+  public User(Long id, String email, String firstName,
+      String lastName, String username, String password,
+      Date createdAt, Set<Role> roles) {
+    this.id = id;
+    this.email = email;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.username = username;
+    this.password = password;
+    this.createdAt = createdAt;
+    this.roles = roles;
+  }
+
+  public User() {}
+  
+  
+}
