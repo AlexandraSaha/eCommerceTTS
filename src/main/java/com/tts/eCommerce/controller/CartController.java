bc9865f7c3cf1d6aa57cbc4c368c79d76bfa77f8
re-becc-k <46948707+re-becc-k@users.noreply.github.com>
2020-07-12 20:44:14 -0400

    Update CartController.java

--- src/main/java/com/tts/eCommerce/controller/CartController.java
+++ src/main/java/com/tts/eCommerce/controller/CartController.java
@@ -1,1 +1,27 @@
 package com.tts.eCommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/storefront")
+public class CartController {
+	@GetMapping("/cart")
+	public String viewCart (Cart cart) {
+		return null;
+		
+	}
+}
+
+@Controller
+@RequestMapping ("/storefront")
+public class MainController {
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAllAttributes("cart", new Cart());
+		return "storefront/index"
+				
+	}
+}
